9ed2182b74a74720a54e088bbdc54a8fa5d1183f
monherjuan <eljukas7w7@gmail.com>
2022-03-03 08:49:13 -0600

    wenas

---
+++ concurrente_01.py
@@ -0,0 +1,77 @@
+# # Las variables en python son de tipo dinamica
+
+# strNombre = "Roberto Eduardo Guzmán Ruiz"
+# fltNumero = 0.01
+# intNumero = 10
+# print(intNumero)
+
+# Comparaciones en python
+# boolData = True
+# if boolData:
+#     print("Si es verdadero")
+# elif boolData == False:
+#     print("No es verdadero")
+
+# sumaUno = 10
+# sumaDos = 11
+
+# resultado = sumaUno + sumaDos
+
+# print(resultado)
+
+# # Cade de String
+# strNombre = 'Roberto Eduardo Guzmán Ruiz'
+# indexStart = 0
+# indexEnd = 18
+# print(strNombre[-5:] + strNombre[-5:])
+
+
+# # Listas
+
+# lista = [1, 2, 3, 4, 5]
+# lista = lista + [6, 7, 8, 9]
+# lista += [10, 11, 12, 13]
+
+# listaAnterior = ["A", "B", "C", "D", "E", "F"]
+# listaNueva = ["G","H", "I", "J", "K", "L", "M"]
+
+# listaTotal = [*listaAnterior, *listaNueva]
+
+# listaTotal.append("O")
+
+# print(listaTotal)
+
+# Sentencias y iteraciones
+# valorEntero = int(input("Ingrese numero"))
+
+# if valorEntero == 0:
+#     print("Es igual a cero")
+# elif valorEntero > 0:
+#     print("Es mayor a cero")
+# elif valorEntero == 10:
+#     print("Es mayor a cero")
+
+# listaNumeros = [1, 2, 3, 4, 5, 6, 7, 8]
+# for num in listaNumeros:
+#     print(num)
+
+# for i in range(30):
+#     print(i)
+
+# autos = { 'Chevrolet': True,  'Nissan': True, 'VMW': False, 'Jeep': False, 'Kia': True}
+# autosNuevos = {}
+
+# for marca, estatus in autos.items():
+#     if estatus == True:
+#         autosNuevos[marca] = estatus
+# print(autosNuevos)
+
+# while True:
+#     pass
+
+# def nueva_funcion(sumaUno):
+#     x = sumaUno + int("10")
+#     print('Resultado: ', x)
+
+
+# nueva_funcion(10)
